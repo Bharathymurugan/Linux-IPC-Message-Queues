a01e967c553f12818a200038b60ef1b4f10e7e7f
bharathy <bharathy@localhost.localdomain>
2026-08-20 05:25:31 +0200

    success

--- msgqueue.c
+++ msgqueue.c
@@ -1,28 +1,4 @@
-# Linux-IPC-Message-Queues
-Linux IPC-Message Queues
-
-# AIM:
-To write a C program that receives a message from message queue and display them
-
-# DESIGN STEPS:
-
-### Step 1:
-
-Navigate to any Linux environment installed on the system or installed inside a virtual environment like virtual box/vmware or online linux JSLinux (https://bellard.org/jslinux/vm.html?url=alpine-x86.cfg&mem=192) or docker.
-
-### Step 2:
-
-Write the C Program using Linux message queues API 
-
-### Step 3:
-
-Execute the C Program for the desired output. 
-
-# PROGRAM:
-
-## C program that receives a message from message queue and display them
-
-```
+// ipcprog.c - Combined Writer/Reader for System V Message Queue
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -64,7 +40,8 @@ int main(int argc, char *argv[]) {
         message.mesg_type = 1;
         printf("Enter Message: ");
         fgets(message.mesg_text, sizeof(message.mesg_text), stdin);
-        message.mesg_text[strcspn(message.mesg_text, "\n")] = 0; // remove newline
+        message.mesg_text[strcspn(message.mesg_text, "\n")] = 0;
+        // remove newline
 
         if (msgsnd(msgid, &message, sizeof(message), 0) == -1) {
             perror("msgsnd");
@@ -92,21 +69,3 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-
-```
-
-
-
-
-
-## OUTPUT
-
-![msgqueue](./images/msgqueue.png)
-
-![ipcs](./images/ipcs.png)
-
-
-
-
-# RESULT:
-The programs are executed successfully.
